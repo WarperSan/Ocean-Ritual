6a293931244a0c03bec475079d33a0fd5a82c92e
WarperSan <78861227+WarperSan@users.noreply.github.com>
2024-08-18 13:58:57 -0400

    Skybox update
    - Remove exposure from the default settings
    - Added a skybox object (STILL WIP)

---
+++ Assets/SkyModule/Scripts/SkyboxManager.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SkyModule
+{
+    public class SkyboxManager : MonoBehaviour
+    {
+        public List<ParallaxObject> parallaxObjects;
+        public MeshRenderer dome;
+        private float offset;
+        public float factor;
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            float elapsed = Time.deltaTime;
+
+            foreach (ParallaxObject item in this.parallaxObjects)
+            {
+                item.origin.Rotate(Vector3.up, item.factor * elapsed);
+            }
+
+            offset += elapsed;// * this.factor;
+
+            dome.material.mainTextureOffset = new Vector2(offset, 0);
+
+            //offset += elapsed * this.factor * 0.25f / 90f;
+            //dome.material.mainTextureOffset = new Vector2(offset, 0);
+        }
+
+        [Serializable]
+        public struct ParallaxObject
+        {
+            public float factor;
+            public Transform origin;
+        }
+    }
+}
