32713b642215a2c9962e4e928b28d6aae2735b9a
ancario <anthonylabbe68@gmail.com>
2024-08-20 10:13:25 -0400

    debutmenu

---
+++ Assets/Game/scene/MenuManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    [SerializeField] GameObject boutonManager;
+
+    Button[] ListeBouton;
+   
+    void Start()
+    {
+
+        int compteur = 1;
+        ListeBouton = boutonManager.GetComponentsInChildren<Button>();
+        foreach (Button b in ListeBouton)
+        {
+            int temporaire = compteur;          
+       
+                b.onClick.AddListener(() => ChangerScene(temporaire));
+
+           
+            compteur++;
+        }
+    }
+
+    
+    
+    public void ChangerScene(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+    
+}
