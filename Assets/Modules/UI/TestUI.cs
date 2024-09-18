569d1985cb9c11896e8e235337a6f31f3c265873
WarperSan <leumas.ecole@gmail.com>
2024-09-18 12:18:23 -0400

    AnimationUI package

---
+++ Assets/Modules/UI/TestUI.cs
@@ -0,0 +1,12 @@
+using DhafinFawwaz.AnimationUILib;
+using UnityEngine;
+
+public class TestUI : MonoBehaviour
+{
+    public AnimationUI animationUI;
+
+    private void Start() {
+        animationUI.Play();
+    }
+    
+}
