b75ab4e927e6392e38b529c62181cecce4289594
ancario <anthonylabbe68@gmail.com>
2024-09-22 23:35:30 -0400

    fishpickable

--- Assets/InventoryStory/Anthony/Script/PickableFish.cs
+++ Assets/InventoryStory/Anthony/Script/PickableFish.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 
 public class PickableFish : MonoBehaviour, IInteractable
 {
+    [SerializeField] string FishName;
+    [SerializeField] int MaxQUantiter;
+    [SerializeField] int Quantiter;
     public void OnClick()
     {
-
+       PoissonData poison =  new PoissonData();
+        poison.nom = FishName;
+        poison.quantiter = Quantiter;
+        poison.quantiterMax = MaxQUantiter;
+        Inventaire.Instance.AddItem(poison);
     }
 }
