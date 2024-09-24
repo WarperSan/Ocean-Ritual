ff4ef7b852a7063ead848ba3dd9453bcc3e61686
WarperSan <leumas.ecole@gmail.com>
2024-09-24 19:43:49 -0400

    Update Controller.cs

--- Assets/Modules/Controller/Scripts/Controllers/Controller.cs
+++ Assets/Modules/Controller/Scripts/Controllers/Controller.cs
@@ -40,7 +40,7 @@ namespace ControllerModule.Controllers
         /// <remarks>
         /// This allows a controller to receive certain updates while being "disabled"
         /// </remarks>
-        protected bool IsEnabled { get; private set; } = true;
+        protected bool IsEnabled { get; private set; } = false;
 
         /// <summary>
         /// Starts using this controller
