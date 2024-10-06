7d132a0f369e30b3896b00c08e1ee7d051d6900e
lolo2178 <lorickdenis@gmail.com>
2024-10-06 17:13:35 -0400

    SharkBehaviorTreeOutline

--- Assets/Modules/Entity/Enemies/Shark/SharkBehaviourTree.cs
+++ Assets/Modules/Entity/Enemies/Shark/SharkBehaviourTree.cs
@@ -1,5 +1,7 @@
 using BehaviourModule.Interfaces;
 using BehaviourModule.Nodes;
+using BehaviourModule.Nodes.Controls;
+using BehaviourModule.Nodes.Generic;
 using UnityEngine;
 
 namespace EntityModule.Enemies
@@ -25,9 +27,48 @@ namespace EntityModule.Enemies
             // Delay between strikes
             // Rushes towards the player and attacks it
 
+            Sequence _root = new();
+            _root += this.AttackSequence();
+            _root += new Parallel(
+                this.RotateSequence(),
+                this.MovementSequence()
+            );
+
             return null;
         }
 
         #endregion
+
+        #region Attack
+
+        private Node AttackSequence()
+        {
+            Sequence attackSequence = new();
+
+            return attackSequence.Alias("Attack Sequence");
+        }
+        #endregion
+
+        #region Movement
+
+        private Node MovementSequence()
+        {
+            Sequence movementSequence = new();
+
+            return movementSequence.Alias("Movement Sequence");
+        }
+        #endregion
+
+        #region Rotation
+        private Node RotateSequence()
+        {
+            Sequence rotateSequence = new();
+            
+
+            return rotateSequence.Alias("Rotate Sequence");
+        }
+        #endregion
+
+
     }
 }
--- Assets/Modules/Entity/Enemies/Shark/SharkEntity.cs
+++ Assets/Modules/Entity/Enemies/Shark/SharkEntity.cs
@@ -4,7 +4,17 @@ namespace EntityModule.Enemies
 {
     public class SharkEntity : EntityBehaviour
     {
+        #region EntityBehaviour
+
         
+
+        /// <inheritdoc/>
+        private void Update()
+        {
+            this.UpdateTree();
+        }
+
+        #endregion
     }
 }
 
