f80c9c58ce00b8df34e7fe580913e8dabcb66129
WarperSan <leumas.ecole@gmail.com>
2024-10-06 12:05:32 -0400

    Fix warnings
    - Duplicate of using
    - GameObject are already nullable, because they are references
    - Classes are already nullable, because they are references

--- Assets/Modules/Gem/Scripts/Gemme/GemHelper.cs
+++ Assets/Modules/Gem/Scripts/Gemme/GemHelper.cs
@@ -1,10 +1,7 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using System.Collections.Generic;
 using static EnumGeneral;
-using UnityEditor;
 
 public static class GemHelper
 {
@@ -13,10 +10,10 @@ public static class GemHelper
 
     public static (int cost, int modifiedSpot, int missingSpot,bool CasseOnlytrue) GetInformationAboutForm(List<bool> formOriginal, List<bool> modifiedForm, int lvlGem)
     {
-        // Vérification si la liste modifiée est plus grande que l'originale
+        // Vï¿½rification si la liste modifiï¿½e est plus grande que l'originale
         if (modifiedForm.Count > formOriginal.Count)
         {
-            throw new ArgumentException("La liste modifiée ne peut pas être plus grande que la liste originale.");
+            throw new ArgumentException("La liste modifiï¿½e ne peut pas ï¿½tre plus grande que la liste originale.");
         }
 
         int modifiedSpot = 0;
@@ -25,37 +22,37 @@ public static class GemHelper
         int originalTrueCount = 0;
         int modifiedTrueCount = 0;
 
-        // Comparaison des booléens dans chaque position des deux listes
+        // Comparaison des boolï¿½ens dans chaque position des deux listes
         for (int i = 0; i < formOriginal.Count; i++)
         {
             // Compte les "true" dans l'original
             if (formOriginal[i]) originalTrueCount++;
 
-            // Compte les "true" dans le modifié uniquement si la case correspondante est aussi "true"
+            // Compte les "true" dans le modifiï¿½ uniquement si la case correspondante est aussi "true"
             if (i < modifiedForm.Count && modifiedForm[i])
             {
                 modifiedTrueCount++;
 
-                // Si la valeur est différente entre l'original et le modifié
-                if (!formOriginal[i]) // Si l'original est false et le modifié est true
+                // Si la valeur est diffï¿½rente entre l'original et le modifiï¿½
+                if (!formOriginal[i]) // Si l'original est false et le modifiï¿½ est true
                 {
                     modifiedSpot++;
                 }
             }
-            else if (formOriginal[i]) // Si l'original est true et le modifié est false
+            else if (formOriginal[i]) // Si l'original est true et le modifiï¿½ est false
             {
                 missingSpot++;
             }
         }
 
-        // Vérification si la liste modifiée contient plus de "true" que l'originale
+        // Vï¿½rification si la liste modifiï¿½e contient plus de "true" que l'originale
         if (modifiedTrueCount > originalTrueCount)
         {
-            throw new ArgumentException("Le nombre de 'true' dans la liste modifiée ne peut pas être supérieur à celui de la liste originale.");
+            throw new ArgumentException("Le nombre de 'true' dans la liste modifiï¿½e ne peut pas ï¿½tre supï¿½rieur ï¿½ celui de la liste originale.");
         }
         bool CasseOnlytrue = modifiedTrueCount == 1 || originalTrueCount == 1;
         missingSpot = missingSpot - modifiedSpot;
-        // Calcul du coût total
+        // Calcul du coï¿½t total
         int totalCost = Mathf.CeilToInt((modifiedSpot * modifiedSpotCost) + (missingSpot * missingSpotCost ));
 
         return (totalCost, modifiedSpot, missingSpot, CasseOnlytrue);
@@ -75,10 +72,10 @@ public static class GemHelper
         // Calculer l'index dans la liste
         int index = YPosition * width + Xposition;
 
-        // Vérifier que l'index est valide
+        // Vï¿½rifier que l'index est valide
         if (index >= 0 && index < ListBool.Count)
         {
-            ListBool[index] = value; // Modifier la valeur à la position spécifiée
+            ListBool[index] = value; // Modifier la valeur ï¿½ la position spï¿½cifiï¿½e
         }
         else
         {
@@ -88,16 +85,16 @@ public static class GemHelper
     public static GemData ConvertGemToGemData(Gem gem)
     {
         List<bool> list = new List<bool>(gem.form.flatForme);
-        // Création d'un nouvel objet GemData
+        // Crï¿½ation d'un nouvel objet GemData
         GemData gemData = new GemData
         {
-            // Copie des propriétés de Gem vers GemData
+            // Copie des propriï¿½tï¿½s de Gem vers GemData
            
             Shape = new FormBool(list, gem.form.width, gem.form.height), 
             GemColorsName = gem.GemColorsName,
             LVL = gem.LVL,
 
-            // Copie des listes des types et quantités
+            // Copie des listes des types et quantitï¿½s
             typeWeapon = new List<TypeQuantity<TypeWeapon>>(gem.typeWeapon),
             typeBoat = new List<TypeQuantity<TypeBoat>>(gem.typeBoat),
             typeNet = new List<TypeQuantity<TypeNet>>(gem.typeNet)
@@ -108,17 +105,17 @@ public static class GemHelper
     
         public static Gem ConvertGemDataToGem(GemData gemData)
         {
-            // Création d'un nouvel objet Gem
+            // Crï¿½ation d'un nouvel objet Gem
             Gem gem = new Gem
             {
-                // Copie des propriétés de GemData vers Gem
+                // Copie des propriï¿½tï¿½s de GemData vers Gem
                 PositionX = 0,
                 PositionZ = 0,
                 form = gemData.Shape,  
                 GemColorsName = gemData.GemColorsName,
                 LVL = gemData.LVL,
 
-                // Copie des listes des types et quantités
+                // Copie des listes des types et quantitï¿½s
                 typeWeapon = new List<TypeQuantity<TypeWeapon>>(gemData.typeWeapon),
                 typeBoat = new List<TypeQuantity<TypeBoat>>(gemData.typeBoat),
                 typeNet = new List<TypeQuantity<TypeNet>>(gemData.typeNet)
--- Assets/Modules/Inventory/Scripts/GemData.cs
+++ Assets/Modules/Inventory/Scripts/GemData.cs
@@ -11,7 +11,7 @@ public class GemData : ItemData
 
     // Shape of the gem (2D boolean array)
     [SerializeField]
-    public FormBool? Shape;
+    public FormBool Shape;
 
     // Name of the gem's color
     [SerializeField] public string GemColorsName;
