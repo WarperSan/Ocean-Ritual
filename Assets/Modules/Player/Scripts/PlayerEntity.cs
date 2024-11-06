3884c47d08e3d2451be3bbe0532cb82101eae122
WarperSan <leumas.ecole@gmail.com>
2024-11-05 20:18:26 -0500

    Player Size Reduction

--- Assets/Modules/Player/Scripts/PlayerEntity.cs
+++ Assets/Modules/Player/Scripts/PlayerEntity.cs
@@ -16,26 +16,7 @@ namespace EntityModule
         #region Entity
 
         /// <inheritdoc/>
-        protected override void OnPostAttack(Projectile source)
-        {
-            Vector3 direction = this.transform.position - source.transform.position;
-            direction.y = 0;
-            direction.Normalize();
-            direction *= 500;
-            direction.y = 300;
-
-            //knockback?
-            //this.GetComponent<Rigidbody>().AddForce(direction);
-        }
-
-        /// <inheritdoc/>
-        protected override void OnDeath(float overDamage)
-        {
-            //tue le joueur
-
-            //soit afficher un menu game over
-            //soit faire respawn le joueur direct apr�s un certain temps
-        }
+        public override bool TakeDamage => false;
 
         #endregion
     }
