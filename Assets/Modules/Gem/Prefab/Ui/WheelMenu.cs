9601b756a46ee5b9df46c36aea72b4689e6aaa7e
ancario <anthonylabbe68@gmail.com>
2024-11-16 19:28:29 -0500

    correctif roue a socle  get stat

--- Assets/Modules/Gem/Prefab/Ui/WheelMenu.cs
+++ Assets/Modules/Gem/Prefab/Ui/WheelMenu.cs
@@ -2,6 +2,8 @@ using BlacksmithModule;
 using UnityEngine.UI;
 using UnityEngine;
 using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
 namespace UIModule.Menus
 {
     public class WheelMenu : AnimatedMenu
@@ -25,12 +27,25 @@ namespace UIModule.Menus
         {
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
-            
+            GetAllUpgradeableItem();
             TransitionCam.Instance.SwitchToCamA();
+
             yield return base.Close();
             
         }
+        private List<Equipment> forgeableItems = new();
+        private void GetAllUpgradeableItem()
+        {
+            //Debug.Log("Nombre d'objets forgeables trouvés : " + forgeableItems.Count);
+            this.forgeableItems = FindObjectsOfType<MonoBehaviour>().OfType<Equipment>().ToList();
+            //Debug.Log("Nombre d'objets forgeables trouvés après recherche : " + forgeableItems.Count);
 
+            foreach (var item in forgeableItems)
+            {
+                item.UpdateStat();
+                Debug.Log("Nom de l'objet : " + ((MonoBehaviour)item).gameObject.name);
+            }
+        }
         public void CloseButton() => UIManager.Close<WheelMenu>();
 
         #region Page
